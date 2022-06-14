6bc15069783eb10721c524112bd9de0e7ba402ac
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-06-14 17:07:16 -0300

    cors url

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://virtual-custom.herokuapp.com/'],
     methods: ["GET","POST"],
     credentials: true
 }));
