fc9418d74ebe430bd02f37a2cefa8e289c23ef94
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2022-06-15 17:59:51 -0300

    corsp

--- app.js
+++ app.js
@@ -23,11 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-    origin: ['https://virtual-custom.herokuapp.com/'],
-    methods: ["GET","POST"],
-    credentials: true
-}));
+app.use(cors());
 app.use(session({
     secret: 'aguante el piti vieja!',
     resave: false,
